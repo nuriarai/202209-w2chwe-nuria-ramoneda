3dc8423561f2be810ea9412780c5e502e1b69788
Núria Ramoneda <nuriarai@gmail.com>
2022-10-08 23:37:58 +0200

    Fixing audit conflicts

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,6 @@
 import Cell from "./gameOfLife/Cell.js";
 import gameBoard from "./gameOfLife/gameBoard.js";
-import loopGame from "./gameOfLife/loopGame.js";
 
-const initGame = loopGame();
-
-console.log(initGame);
+const gray = new Cell(1, 2, 0);
+console.log(gray);
+console.log(gameBoard(3));
