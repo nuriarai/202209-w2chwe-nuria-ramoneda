23649f000f8b057406868e952eb62560cb01c589
Núria Ramoneda <nuriarai@gmail.com>
2022-10-08 23:24:44 +0200

    Chang names and move files of js structure

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+import Cell from "./gameOfLife/Cell.js";
+import gameBoard from "./gameOfLife/gameBoard.js";
+import loopGame from "./gameOfLife/loopGame.js";
+
+const initGame = loopGame();
+
+console.log(initGame);
--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-import Cell from "./Cell/Cell.js";
-
-const a = new Cell(1, 1, 1);
-console.log(a);
