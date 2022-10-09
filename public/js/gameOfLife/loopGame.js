4f93dbc5407eca1fa96005c01e2568f7b6bdaffd
Núria Ramoneda <nuriarai@gmail.com>
2022-10-09 11:02:47 +0200

    Add function neigboursCheck

--- public/js/gameOfLife/loopGame.js
+++ public/js/gameOfLife/loopGame.js
@@ -2,31 +2,45 @@ import gameBoard from "./gameBoard.js";
 
 const boardSize = 3;
 
-const neighboursCheck = (currentCell) => {
-  if (currentCell.status === 0) {
+const neighboursCheck = (xx, yy, status) => {
+  if (status === 0) {
     // has 3 neighbours alive
-    for (let i = 0; i < 8; i++) {
-      const neighboursBoard = gameBoard(boardSize);
+    // for (let i = 0; i < 8; i++) {
+    const neighboursBoard = gameBoard(boardSize);
+
+    if ((xx > 0 && xx < 3) || (yy > 0 && yy < 3)) {
+      const right = neighboursBoard[xx][yy + 1];
+      const diagonalRightBottom = neighboursBoard[xx + 1][yy + 1];
+      const bottom = neighboursBoard[xx + 1][yy];
+      const diagonalLeftBottom = neighboursBoard[xx + 1][yy - 1];
+      const left = neighboursBoard[xx][yy - 1];
+      const diagonalLeftTop = neighboursBoard[xx - 1][yy - 1];
+      const top = neighboursBoard[xx - 1][yy];
+      const diagonalRightTop = neighboursBoard[xx - 1][yy + 1];
     }
+
+    console.log("a la dret i a baix en diagonal");
+    console.log(neighboursBoard[xx + 1][yy + 1]);
+
+    console.log("a l'esquerra i a baix en diagonal");
+    console.log(neighboursBoard[xx - 1][yy - 1]);
+
+    console.log("current:");
+    console.log(neighboursBoard[xx][yy]);
+    console.log("costat dreta");
+    console.log(neighboursBoard[xx][yy + 1]);
+    console.log("a baix");
+    console.log(neighboursBoard[xx + 1][yy]);
+    /*     console.log("a l'esquerra i a baix en diagonal");
+    console.log(neighboursBoard[xx - 1][yy - 1]); */
+    console.log("costat esquerra");
+    console.log(neighboursBoard[xx][yy - 1]);
   }
+  return 1;
 };
 
 const loopGame = () => {
   const board = gameBoard(boardSize);
-
-  for (let x = 0; x < boardSize; x++) {
-    for (let y = 0; y < boardSize; y++) {
-      const currentCell = board[x][y];
-
-      if (currentCell.status === 0) {
-        const result = neighboursCheck(currentCell);
-      }
-      if (board[x][y] !== undefined) {
-        // board[x][y].status = 8;
-        console.log(board[x][y]);
-      }
-    }
-  }
 
   console.log(board);
 };
