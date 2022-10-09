610c6fcb554c4e8c1ce3aadd8e04e5093a06411c
Núria Ramoneda <nuriarai@gmail.com>
2022-10-09 13:19:50 +0200

    Remove console logs and rewritre path to GameBoard.js

--- public/js/gameOfLife/loopGame.js
+++ public/js/gameOfLife/loopGame.js
@@ -4,8 +4,6 @@ const boardSize = 3;
 
 const neighboursCheck = (xx, yy, status) => {
   if (status === 0) {
-    // has 3 neighbours alive
-    // for (let i = 0; i < 8; i++) {
     const neighboursBoard = GameBoard(boardSize);
 
     if ((xx > 0 && xx < 3) || (yy > 0 && yy < 3)) {
@@ -18,30 +16,12 @@ const neighboursCheck = (xx, yy, status) => {
       const top = neighboursBoard[xx - 1][yy];
       const diagonalRightTop = neighboursBoard[xx - 1][yy + 1];
     }
-
-    /*     console.log("a la dret i a baix en diagonal");
-    console.log(neighboursBoard[xx + 1][yy + 1]);
-
-    console.log("a l'esquerra i a baix en diagonal");
-    console.log(neighboursBoard[xx - 1][yy - 1]);
-
-    console.log("current:");
-    console.log(neighboursBoard[xx][yy]);
-    console.log("costat dreta");
-    console.log(neighboursBoard[xx][yy + 1]);
-    console.log("a baix");
-    console.log(neighboursBoard[xx + 1][yy]);
-        console.log("a l'esquerra i a baix en diagonal");
-    console.log(neighboursBoard[xx - 1][yy - 1]); 
-    console.log("costat esquerra");
-    console.log(neighboursBoard[xx][yy - 1]); */
   }
   return 1;
 };
 
 const loopGame = () => {
   const board = new GameBoard(3, 3);
-  // console.log(board);
 };
 
 export default loopGame;
--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,5 @@ import GameBoard from "./gameOfLife/GameBoard.js";
 import loopGame from "./gameOfLife/loopGame.js";
 
 const gray = new Cell(1, 2, 0);
-// console.log(gray);
-// console.log(GameBoard(3, 3));
-
 const initGame = loopGame();
-// console.log(initGame);
-
 const newBoard = new GameBoard(3, 3);
-/* console.log(newBoard.createBoard());
-console.log(newBoard.board.length); */
