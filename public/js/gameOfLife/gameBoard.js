88c1a02f649885ba561ab12758fa250b979c15bb
Núria Ramoneda <nuriarai@gmail.com>
2022-10-09 17:07:56 +0200

    Add new method to print the cells in their initial position and status

--- public/js/gameOfLife/gameBoard.js
+++ public/js/gameOfLife/gameBoard.js
@@ -8,6 +8,7 @@ class GameBoard {
   constructor(rows, columns) {
     this.rows = rows;
     this.columns = columns;
+    this.createBoard();
   }
 
   createBoard() {
@@ -23,9 +24,25 @@ class GameBoard {
         board[x] = position;
       }
     }
-
     this.board = board;
     return this.board;
+  }
+
+  printBoard() {
+    const boardToPrint = [];
+    for (let x = 0; x < this.rows; x++) {
+      const rowToPrint = [];
+      for (let y = 0; y < this.columns; y++) {
+        if (this.board[x][y].status) {
+          rowToPrint[y] = "X";
+          boardToPrint[x] = rowToPrint;
+        } else {
+          rowToPrint[y] = "-";
+          boardToPrint[x] = rowToPrint;
+        }
+      }
+    }
+    return boardToPrint;
   }
 }
 
